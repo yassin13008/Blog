15c13e123c91c9739dd5f1ab1f90b626d004aae7
Yassin Hammami <yas.hammami@gmail.com>
2023-01-17 17:23:28 +0100

    traitement des données du formulaires => arret apres customisation des champs

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Post;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,10 +13,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PostController extends AbstractController
 {
     #[Route("/post/{slug}", name:"post.index")]
-public function index(Post $post): Response
+public function index(Post $post,Request $request): Response
 {
-   return $this->render('/post/index.html.twig', [
-       'post' => $post
-   ]);
-    }
+    $form = $this->createForm(CommentType::class);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+        dd($form->getData());
+   }
+
+    return $this->render('post/index.html.twig', [
+        'post' => $post,
+        'form' => $form->createView()
+    ]);
+    
+}
+
 }
