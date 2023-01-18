8312a5e64fae9eec4662e05315df25b30c960239
Yassin Hammami <yas.hammami@gmail.com>
2023-01-18 12:25:21 +0100

    Chap 8 => Protéger l'ajout de commentaire => la connexion est faite

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,6 +27,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'security.logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        
     }
 }
