9bbe3c87c57fbc5f16b4a462a7bef4fc31881717
Yassin Hammami <yas.hammami@gmail.com>
2023-01-18 12:06:06 +0100

    Chap 8 => Menu utilisateur à faire

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\PostFactory;
+use App\Factory\UserFactory;
 use App\Factory\CommentFactory;
 use App\Factory\CategoryFactory;
 use Doctrine\Persistence\ObjectManager;
@@ -15,6 +16,12 @@ class AppFixtures extends Fixture
         CategoryFactory::new()->createMany(5);
         PostFactory::new()->createMany(10);
         CommentFactory::new()->createMany(50);
+
+        UserFactory::new()->createMany(10);
+        UserFactory::createMany(10);
+        UserFactory::createOne(['email' => 'user@gmail.com']);
+
+        
 
   // Enregistrement dans la base de données
   $manager->flush();
